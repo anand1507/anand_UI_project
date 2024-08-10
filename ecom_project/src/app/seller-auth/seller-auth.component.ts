5a142d4d14a39692870b55adc77c8f423351355a
Anand kumar <anandgupta14735@gmail.com>
2024-08-10 18:47:45 +0530

    seller-home created

---
+++ ecom_project/src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { SellerService } from '../services/seller.service';
+import { Router } from '@angular/router';
+import { SignUp } from '../data-type';
+@Component({
+  selector: 'app-seller-auth',
+  templateUrl: './seller-auth.component.html',
+  styleUrl: './seller-auth.component.css'
+})
+export class SellerAuthComponent implements OnInit{
+  constructor(private seller:SellerService,private router:Router){}
+  ngOnInit(): void {
+  }
+
+  signUp(data:SignUp):void{
+    console.warn(data);
+    this.seller.userSignUp(data).subscribe((result)=>{
+      if (result){
+        this.router.navigate(['seller-home'])
+      }
+    });
+  }
+}
